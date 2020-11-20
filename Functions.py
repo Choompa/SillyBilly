8a051dbfb8c35bdd152997fabaf9c27f141aa5e7
Choompa <72717648+Choompa@users.noreply.github.com>
2020-11-19 21:35:10 -0500

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,9 @@
+def swap():
+    dataa = ('potato')
+    datab = ('onions')
+    x = input('Do You Want To Open dataa which is potato or datab which is onions?')
+    if x == dataa:
+        print(datab + ' HAHAHA')
+    else:
+        print(dataa + ' HAHAHA')
+swap()
